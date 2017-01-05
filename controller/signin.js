88b6b6c184fc02aa141ad75a5b38635cf31fd1e5
YeepLv.Mac <yeep.for.dev@gmail.com>
2017-01-05 17:15:34 +0800

    finish html page

--- controller/signin.js
+++ controller/signin.js
@@ -1,11 +1,13 @@
 module.exports = {
     'POST /signin':async (ctx,next)=>{
-        var username = ctx.request.username,
-            password = ctx.request.password;
+        var username = ctx.request.body.username,
+            password = ctx.request.body.password;
         if(password === '12345'){
-            ctx.render('signin-ok.html');
+            ctx.render('chatroom.html');
         }else{
             ctx.render('signin-fail.html');
         }
+
+        await next();
     }
 }
